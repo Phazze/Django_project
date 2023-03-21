1c85dd76770b7e8f4fa8c536f5997215b843396a
Phazze <nekrashevich.gleb@gmail.com>
2023-03-21 20:32:57 +0300

    Create page and methods

--- tutorial/main/views.py
+++ tutorial/main/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Main page of internate', 'tasks': tasks})
 
 
 def about(request):
     return render(request,'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Form is not good "
+
+    form = TaskForm()
+    contex = {
+        'form': form,
+        'error': error
+    }
+    return render(request,'main/create.html', contex)
